7fb08bcab8308e169c857ea5ff20b6c5ff5cc742
admin <admin@admins-MacBook-Pro.local>
2020-05-23 00:16:46 +0200

    aaa

--- scripts/newUser.sh
+++ scripts/newUser.sh
@@ -18,8 +18,8 @@ cd /home/"$USER"
 mkdir /home/"$USER"/.ssh
 touch /home/"$USER"/.ssh/authorized_keys
 wget "$KEY_URL"
-cat key.pub >> /home/"$USER"/.ssh/authorized_keys
-rm key.pub
+cat pub.key >> /home/"$USER"/.ssh/authorized_keys
+rm pub.key
 chown -R "$USER":"$USER" /home/"$USER"/.ssh
 chmod 600 /home/"$USER"/.ssh/authorized_keys
 chmod 700 /home/"$USER"/.ssh
